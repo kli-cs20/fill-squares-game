d76242a53bfacb86f32b203eb3805ad064cabe27
kendallli587 <kendallli587@gmail.com>
2022-07-12 10:03:47 -0600

    PUZZLE 1

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 // Global Constants
 const NUM_ROWS = 6;
 const NUM_COLS = 6;
+const resultOne = document.getElementById("result");
 
 // Create array to represent a grid
 let grid = createGridArray();
@@ -31,26 +32,56 @@ document.addEventListener("keydown", movePlayer);
 
 function movePlayer(e) {
     if (e.keyCode === 39) { // Right arrow key
-        updatePlayer(player.row, player.col + 1);
+        if (player.col + 1 < 6) {
+            updatePlayer(player.row, player.col + 1);
+        }
     } else if (e.keyCode === 37) { // Left arrow key
-        updatePlayer(player.row, player.col - 1);
+        if (player.col - 1 >= 0) {
+            updatePlayer(player.row, player.col - 1);
+        }
     } else if (e.keyCode === 38) { // up arrow key
-        updatePlayer(player.row - 1, player.col);
+        if (player.row - 1 >= 0) {
+            updatePlayer(player.row - 1, player.col);  
+        }
     } else if (e.keyCode === 40) { // Down arrow key
-        updatePlayer(player.row + 1, player.col);
+        if (player.row + 1 < 6) {
+            updatePlayer(player.row + 1, player.col);
+        }
     }
+
+    // Check if puzzle is completed
+    if (grid[endGoal.row][endGoal.col] === 2) {
+        resultOne.innerHTML = "Congratulations, you completed Puzzle 1!"
+    }
+
 }
 
 function updatePlayer(newRow, newCol) {
-    // Update player location
-    player.row = newRow;
-    player.col = newCol;
+    function checkCollisions() {
+        return grid[newRow][newCol] !== 1 && grid[newRow][newCol] !== 2;
+    }
 
-    // Update class and grid
-    let cellId = "cell" + player.row + "-" + player.col;
-    document.getElementById(cellId).classList.add("player");
+    if (checkCollisions()) {
+        // Update player location
+        player.row = newRow;
+        player.col = newCol;
+            
+        // Update class and grid
+        let cellId = "cell" + player.row + "-" + player.col;
+        document.getElementById(cellId).classList.add("player");
 
-    grid[player.row][player.col] = 2;
+        grid[player.row][player.col] = 2;
+    }
 }
 
+function checkIfComplete() {
+    for (let row = 0; row < 6; row++) {
+        for (let col = 0; col < 6; col++) {
+            if (grid[row][col] !== 0) {
+                return true
+            }
+            return false
+        }
+    }
+}
 
